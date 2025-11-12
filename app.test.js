8d4b9d23ca3ddbefddb3fe3359bfe8974f79094b
unknown <Administrador@UNI-LABF-7460.fuvs.br>
2025-11-11 21:58:27 -0300

    alteracao da mensagem

--- app.test.js
+++ app.test.js
@@ -5,6 +5,6 @@ describe('Teste da aplicação Olá Mundo DevOps', () => {
   it('Deve retornar Olá Mundo DevOps!', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
-    expect(response.text).toBe('Olá Mundo DevOps!');
+    expect(response.text).toBe('Olá Mundo DevOps exercicio em aula!');
   });
 });
