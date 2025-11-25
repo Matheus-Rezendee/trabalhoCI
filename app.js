c04894f2016c4f7ea3ed13140c966c39eab31be7
MAtheus Fernandes Rezende <eumatheusrezende01@hotmail.com>
2025-11-25 14:33:39 -0300

    Mensagem alterada para nova versão Olá, Mundo DevOps + vulnerabilidades corrigidas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.status(200).send("Olá Mundo DevSecOps");
+  res.status(200).send("Olá Mundo DevOps tudo bem?");
 });
 
 module.exports = app; // Exportamos o app
